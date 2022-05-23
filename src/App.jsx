3297729e36f22e2d070a3a4c9086c845edea4fd0
Haranmes <soliman.harames15@gmail.com>
2022-05-23 17:09:40 +0200

    new

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,27 @@ import { useRef } from 'react';
 
 import dark from './images/dark.jpg'
 import Gretche from './images/Gretchen.jpg'
-
+import black from './images/black.jpg'
  
 function App() {
   const ref = useRef();
     return(
-        <div>
-        <Parallax pages={4} ref={ref}>
-       
+        <div id="App">
+        <Parallax pages={5} ref={ref}>
             <ParallaxLayer id="chaos"
             offset={0}
             speed={0.5}
             factor={2}
             style={{
                 backgroundImage: `url(${dark})`,
-                backgroundSize: 'cover'
+                backgroundSize: 'cover',
             }}
             >
             </ParallaxLayer>
             <ParallaxLayer
             offset={0.3}
             speed={1}
-            factor={6}
-            
+            factor={2}
             style={{
                 backgroundImage: `url(${Gretche})`,
                 left: '15%'
@@ -43,13 +41,29 @@ function App() {
                     }}
                 />
             )}
-        
         ></ParallaxLayer>
+         <ParallaxLayer
+        offset={1.3}
+        speed={0.5}
+        factor={3}
+        style={{ 
+            backgroundImage: `url(${black})`,
+            backgroundSize: 'cover',
+            position: 'fixed'
+        }}
+        >
+            <h3 id="h2">"Meine Ruh ist hin, <br></br>
+                        Mein Herz ist schwer; <br></br>
+                        Ich finde sie nimmer <br></br>
+                        und nimmermehr." - Gretchen,  Gretchens Stube</h3>
+        </ParallaxLayer>
           <ParallaxLayer
           style={{ textAlign: 'center' , position: 'fixed', top: '10%'}}
+          speed={0.5}
         >
-          <h3 id="h3">Untergangs Gretchens</h3>
+          <h3 id="h3">Untergang Gretchens</h3>
         </ParallaxLayer>
+        
         </Parallax>
     </div>
     );
