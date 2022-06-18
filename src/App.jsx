7a26abbbee55785828b247b80f74d9a61e25eda1
Haranmes <soliman.harames15@gmail.com>
2022-06-18 21:43:29 +0200

    Anfang vom Ende
    hab den Übergang zum Ende gemacht
    TODO:
    -  Zelle mit Gretchen (Untergang + Mord an Kind)
    -  Button zum Tagebuch

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,24 @@ import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import { useRef } from 'react';
 
 import './App.css'
-import dark from './images/dark.jpg';
-import Trees from './images/Trees.png'
+
 import Gretche from './images/Gretchen.jpg'
 import black from './images/black.jpg'
-
+import faust from './images/Faust.png'
+import gretchen from './images/Gretchen.png'
 import street from './images/street.jpg'
-import sky from './images/sky.jpg'
+import zimmer from './images/Schlafzimmer.jpg'
 import two from './images/two.jpg'
-
-
-
+import rain from './images/rain.mp4'
+import cell from './images/GretchenInKerker.jpg'
+import kussen from './images/GretchenFaustKuss.jpg'
 const App = () => {
    
     const ref = useRef();
     return(
         
         <div id="Home"> 
-        <div>
-       
-       
-        
-        </div>
-       
-       
-       
-        <Parallax pages={8} ref={ref}>
+        <Parallax pages={11} ref={ref}>
             <ParallaxLayer id="chaos"
             offset={0}
             speed={0.5}
@@ -49,13 +41,13 @@ const App = () => {
                 top: '6%',
                 left: '10%'
             }}
-            speed={1}
+            speed={0.7}
             >
                 <h3 id="h3">Untergang Gretchens</h3>
             </ParallaxLayer>
             <ParallaxLayer id="gretchen"
             offset={0.3}
-            speed={1}
+            speed={0.7}
             factor={2}
             style={{
                 backgroundImage: `url(${Gretche})`,
@@ -72,29 +64,67 @@ const App = () => {
                 backgroundImage: `url(${black})`,
                 backgroundSize: 'cover',
                 textAlign: 'center' , 
+                
             }}
-            >
-                <h3 id="h2">"Meine Ruh ist hin, <br></br>
+            > 
+               
+                <h3 id="h2" style={{left: "30%"}}>"Meine Ruh ist hin, <br></br>
                             Mein Herz ist schwer; <br></br>
                             Ich finde sie nimmer <br></br>
-                            und nimmermehr." - <br></br>Gretchen,  Gretchens Stube</h3>
+                            und nimmermehr." - <br></br>Gretchen,  Gretchens Stube
+                </h3>
                             
             </ParallaxLayer>
-            
             <ParallaxLayer
-            offset={1.2}
+            offset={2}
             speed={0.5}
-            factor={1}
-            style={{ 
-                backgroundColor: `black`,
-                backgroundSize: 'cover',
-                textAlign: 'center' , position: 'fixed', top: '20%'
-            }}
-            ><h3 id="h2">Gretchen musste vieles Durchmachen bis sie verrückt im Gefängnis landet.<br></br>
-                Ihre Naivität und Kindlichkeit waren Gründe ihres Verderbens...
-            </h3></ParallaxLayer>
+            factor={2}
+            style={{
+                background: 'linear-gradient(to bottom, #000, #53516e)',
+                backgroundSize: 'cover'
+            }}>
+            </ParallaxLayer>
             <ParallaxLayer
-            offset={3}
+            offset={2.6}
+            speed={0.5}
+            style={{
+                backgroundSize: 'contain',
+                top: "10%",
+                height:"20% auto",
+                borderWidth: '1 solid #fff'
+            }}>
+                 <video loop autoPlay muted id="video">
+                    <source
+                        src={rain}
+                        type={"video/mp4"}
+                    />
+                    Your browser does not support the video tag.
+                </video>
+            </ParallaxLayer>
+            <ParallaxLayer
+            offset={3.5}
+            speed={0.7}
+            style={{ 
+                
+                
+                textAlign: 'center' ,
+            }}
+            >
+                
+                <h3 id="h1" style={{display: "grid", placeItems: "center"}}>Gretchen musste vieles Durchmachen bis sie verrückt im Gefängnis landet.<br></br>
+                            Es begann alles mit ein vermeindliches Treffen...
+                </h3>
+            </ParallaxLayer>
+            <ParallaxLayer
+            offset={3.7}
+            speed={0.7}
+            style={{
+                backgroundImage: `url(${cell})`,
+                backgroundSize: 'contain',
+                backgroundPosition: 'center'
+            }}></ParallaxLayer>
+            <ParallaxLayer
+            offset={5}
             speed={0.5}
             factor={2}
             id="street"
@@ -102,17 +132,123 @@ const App = () => {
                 backgroundImage: `url(${street})`,
                 backgroundSize: 'contain',
                 backgroundPosition: 'center',
-                backgroundColor: 'white'
                 
             }}>
-            <a
+               
+            {/*<a
             href="https://haranmes.github.io/book-for-faust/"
             target="_blank"
             rel="noopener noreferrer"
             >
-                <button class="btn-11 custom-btn">dsafdf</button>
-            </a>
+                <button class="btn-11 custom-btn">Gretchens Tagebuch</button>
+            </a>*/}
                 
+            </ParallaxLayer>
+            <ParallaxLayer
+            offset={5.89}
+            speed={0.7}
+            style={{
+                left: "30%",
+            }}
+            >
+                 <h id="h4" style={{fontSize: "4rem", textAlign: "center", }}>Eines Tages auf einer Straße... </h>
+            </ParallaxLayer>
+            <ParallaxLayer
+            offset={5.9}
+            speed={0.5}
+            style={{
+                backgroundImage: `url(${faust})`,
+                position: 'absolute',
+                left: '30%',
+               
+            }}> 
+                <div className="bubble2" style={{left: '24%', top: "30%", textAlign: "center", }}>
+                    <h style={{color: "white", fontFamily: 'Soft Compound'}}>O schönes Weib, kann ich ihnen Geleit anbieten?</h>
+                </div>
+            </ParallaxLayer>
+            <ParallaxLayer
+            offset={5.9}
+            speed={0.5}
+            style={{
+                backgroundImage: `url(${gretchen})`,
+                position: 'absolute',
+                left: '15%'
+            }}>
+                <div className="bubble2" style={{left: '10%', top: "30%", textAlign: "center", }}>
+                    <h style={{color: "white", fontFamily: 'Soft Compound'}}>Ich muss ablehnen werter Herr. Ich bin weder Fräulein, weder schön...</h>
+                </div>
+            </ParallaxLayer>
+            <ParallaxLayer
+            offset={6.7}
+            speed={0.5}
+            factor={0.5}
+            style={{
+                backgroundColor: 'black',
+                backgroundSize: 'cover'
+            }}>
+                <h style={{color: 'white', fontSize: "4rem", display: "grid", placeItems: "center"}}>Doch dann soll etwas schicksalhaftes geschehen...</h>
+            </ParallaxLayer>
+            <ParallaxLayer
+            offset={6.99}
+            speed={0.5}
+            factor={2}
+            style={{
+                backgroundColor: "#ff99cc",
+                background: "linear-gradient(to right, rgba(255, 153, 204, 0.5), rgba(255, 102, 102, 0.6))",
+            }}>
+            </ParallaxLayer>
+            <ParallaxLayer
+            offset={7}
+            speed={0.7}
+            style={{
+                left: "30%"
+            }}>
+                <h id="h4" style={{fontSize: "4rem", textAlign: "center", }}>Im Garten der Nachbarin</h>
+            </ParallaxLayer>
+            <ParallaxLayer
+            offset={7.2}
+            speed={0.7}
+            style={{
+                left: "31%"
+            }}>
+                <div id="hearts-alpaca" className="hearts">
+                    <div className="heart"></div>
+                    <div className="heart"></div>
+                    <div className="heart"></div>
+                    <div className="heart"></div>
+                </div>
+            </ParallaxLayer>
+            <ParallaxLayer
+            offset={7.5}
+            speed={0.7}
+            style={{
+                backgroundImage: `url(${kussen})`,
+                left: "30%"
+            }}>
+            </ParallaxLayer>
+            <ParallaxLayer
+            offset={7.6}
+            speed={0.7}
+            style={{
+                left: "40%"
+            }}
+            >
+                <h id="h4" style={{fontSize: "1rem", textAlign: "center", color: "white"}}>Jetzt nimmt das Geschehen seinen Lauf...</h>
+            </ParallaxLayer>
+            <ParallaxLayer
+            offset={8}
+            speed={0.5}
+            style={{
+               margin: "0",
+               padding: "0",
+               fontFamily:  "Poppins, sans-serif",
+            }}>
+                <div id="horror">
+                    <div id="wrapper">
+                        <h1 class="glitch" data-text="Der Anfang des Endes">Der Anfang des Endes</h1>
+                        <p class="red">Gretchens</p>
+                    </div>
+                </div>
             </ParallaxLayer>
         </Parallax>
         
